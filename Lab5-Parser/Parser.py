e4795b734c23f4de003b80d1cc986e633bc54a44
912-Cireap-Bogdan <bogdan.cireap@gmail.com>
2023-01-11 08:50:30 +0200

    final commit for parser

--- Lab5-Parser/Parser.py
+++ Lab5-Parser/Parser.py
@@ -8,9 +8,54 @@ class ParsingLL1:
         self.grammar = Grammar(file=grammar)
         self.sequence = self.read_sequence(file=sequence)
         self.out = out
-        
-        pass
+        self.first_set = self.First(self.grammar.S[0], {}, [])
 
+        self.follow_set = self.Follow({}, self.first_set)
+        self.parsing_table = self.create_parsing_table()
+
+
+    def create_parsing_table(self):
+        """
+        for each production A -> alpha in your grammar:
+            daca am S->
+          for each terminal t in FIRST(alpha):
+            add A -> alpha to M[A, t]
+          if epsilon is in FIRST(alpha):
+            for each terminal b in FOLLOW(A):
+      add A -> alpha to M[A, b]
+        :return:
+        """
+        parsing_table = {}
+        for nonterminal in self.grammar.N:
+            parsing_table[nonterminal] = {}
+            for terminal in self.grammar.E:
+                parsing_table[nonterminal][terminal] = []
+            parsing_table[nonterminal]['$'] = []
+
+
+        for key in self.grammar.P.keys():
+
+            aux = self.grammar.P[key]
+            for nonterminal in aux:
+                if nonterminal.isupper():
+                    aux_first = self.first_set[nonterminal]
+                    for _ in aux_first:
+                        if _ != '@':
+                            parsing_table[key][_].append(nonterminal)
+                        if '@' in aux_first and '$' not in parsing_table[key]['$']:
+                            parsing_table[key]['$'] = ['$']
+                else:
+                    aux_first = self.first_set[key]
+                    for _ in aux_first:
+                        if _ != '@' and _ not in parsing_table[key][_]:
+                            parsing_table[key][_].append(_)
+                        if '@' in aux_first and '$' not in parsing_table[key]['$']:
+                            follow_aux = self.follow_set[key]
+                            for item in follow_aux:
+                                if item not in parsing_table[key][item]:
+                                    parsing_table[key][item].append(item)
+
+        return parsing_table
 
     def read_sequence(self, file):
 
@@ -23,7 +68,7 @@ class ParsingLL1:
         print("sequence: ",seq)
         return seq
 
-    def First(self, grammar_symbol,answer,skip):
+    def First(self, grammar_symbol, answer, skip=None):
         """
         Rules to compute FIRST set:
         If x is a terminal, then FIRST(x) = { ‘x’ }
@@ -33,11 +78,10 @@ class ParsingLL1:
             If FIRST(Y1) contains Є then FIRST(X) = { FIRST(Y1) – Є } U { FIRST(Y2) }
             If FIRST (Yi) contains Є for all i = 1 to n, then add Є to FIRST(X).
         """
-
-        # daca is upper treve calculat First(X)
-
+        if skip is None:
+            skip = []
         if grammar_symbol[0].islower() and len(grammar_symbol) == 1:
-            return {grammar_symbol[0] : [grammar_symbol[0]]}
+            return {grammar_symbol[0]: [grammar_symbol[0]]}
 
         if grammar_symbol[0].isupper():
             if grammar_symbol in self.grammar.S:
@@ -46,17 +90,13 @@ class ParsingLL1:
                 for item in self.grammar.P[grammar_symbol]:
                     list_to_calculate.append(item)
 
-
                 for item in list_to_calculate:
-                    self.First(item,answer,skip)
-
+                    self.First(item, answer, skip)
             else:
                 for item in self.grammar.P[grammar_symbol]:
                     if grammar_symbol not in answer:
                         answer[grammar_symbol] = []
                     answer[grammar_symbol].append(item)
-
-
 
         for key in answer:
             if key not in self.grammar.S:
@@ -64,8 +104,8 @@ class ParsingLL1:
                     for item in answer[key]:
                         if '@' in answer[key]:
                             if item != '@':
-                                answer[self.grammar.S[0]].append(item)
-
+                                aux = self.grammar.S[0]
+                                answer[aux].append(item)
                         else:
                             answer[self.grammar.S[0]].append(item)
                             for item in self.grammar.P.keys():
@@ -77,8 +117,7 @@ class ParsingLL1:
 
         return answer
 
-
-    def Follow(self, grammar_symbol, answer,first_set):
+    def Follow(self, answer,first_set):
         """
         1) FOLLOW(S) = { $ }   // where S is the starting Non-Terminal
         2) If A -> pBq is a production, where p, B and q are any grammar symbols,
@@ -87,78 +126,90 @@ class ParsingLL1:
         4) If A->pBq is a production and FIRST(q) contains Є,
            then FOLLOW(B) contains { FIRST(q) – Є } U FOLLOW(A)
         """
-        if grammar_symbol[0].isupper():
-            if grammar_symbol in self.grammar.S:
-                answer[grammar_symbol] = ['$']
-                list_to_calculate =[]
-                for item in self.grammar.P[grammar_symbol]:
-                    list_to_calculate.append(item)
-                    answer[item] = []
+        answer = {nonterminal: [] for nonterminal in self.grammar.N}
+        for nonterminal in self.grammar.N:
+            if nonterminal.isupper():
+                if nonterminal in self.grammar.S:
+                    answer[nonterminal].append('$')
 
-                for item in list_to_calculate:
-                    self.Follow(item, answer,first_set)
+                # for nonterm, rhs in self.grammar.P.items():
+                else:
+                    for _, s_rhs in self.grammar.P.items():
+                        if nonterminal in s_rhs:
+                            aux = s_rhs.index(nonterminal)
+                            if aux + 1 == len(s_rhs):
+                                answer[nonterminal].append('$')
+                                break
+                            else:
+                                aux_for_first_set = s_rhs[aux+1]
+                            if '@' in self.first_set[aux_for_first_set]:
+                                for item in self.first_set[aux_for_first_set]:
+                                    answer[nonterminal].append(item)
+                                answer[nonterminal].remove('@')
+                                for item in answer[_]:
+                                    answer[nonterminal].append(item)
 
-            else:
-                placeholder =[]
-                # for aux in self.grammar.N:
-                #     if aux not in self.grammar.S and aux != grammar_symbol:
-                #         placeholder.append(aux)
-                index = self.grammar.N.index(grammar_symbol)
-                placeholder = self.grammar.N[index+1:]
-                answer[grammar_symbol] = placeholder
-
-                aux_answer = []
-                stop = False
-                for item in answer[grammar_symbol]:
-                    for aux_item in first_set[item]:
-                        if "@" in first_set[item]:
-                            if aux_item != "@":
-                                aux_answer.append(aux_item)
-                        else:
-                            aux_answer.append(aux_item)
-                            answer[grammar_symbol] = aux_answer
-                            return answer
-
-                aux_answer.append('$')
-
-
-                answer[grammar_symbol] = aux_answer
-
-
-
-
+                            else:
+                                answer[nonterminal] = self.first_set[aux_for_first_set]
 
         return answer
 
-    #LL(1) - functions FIRST, FOLLOW
-    # First(): If there is a variable, and from that variable,if we try to drive all the strings then the beginning Terminal Symbol is called the First.
-    # Follow(): What is the Terminal Symbol which follows a variable in the process of derivation.
+    def parse(self):
+        stack = []
+        stack.append(self.grammar.S[0])
+        sequence = [item for item in self.sequence[0]]
+        # Initialize the current symbol to be the first symbol in the sequence
+        i = 0
+        current_symbol = sequence[i]
+        iteration = 0
+        while stack:
+            # Get the top symbol on the stack
+            top_symbol = stack[-1]
+            # If the top symbol is a terminal
+            if top_symbol in self.grammar.E:
+                # If it matches the current symbol in the sequence, move to the next symbol
+                if top_symbol == current_symbol:
+                    stack.pop()
+                    i += 1
+                    if i < len(sequence):
+                        current_symbol = sequence[i]
+                    if i == len(sequence) -1:
+                        return True
+                # If it does not match, the sequence is not accepted
+                else:
+                    return False
+            # If the top symbol is a nonterminal
+            else:
+                # Get the production for this nonterminal
+                production = self.parsing_table[top_symbol][current_symbol]
+                # If there is no production, the sequence is not accepted
+                if not production:
+                    return False
+                # Replace the nonterminal with the production
+                # stack.pop()
 
-    def parse_algorithm(self):
-        """
-        For each production A –> α. (A tends to alpha)
-
-        1.Find First(α) and for each terminal in First(α), make entry A –> α in the table.
-        2.If First(α) contains ε (epsilon) as terminal, then find the Follow(A) and for each terminal in Follow(A), make entry A –>  ε in the table.
-        3.If the First(α) contains ε and Follow(A) contains $ as terminal, then make entry A –>  ε in the table for the $.
-
-        To construct the parsing table, we have two functions:
-            In the table, rows will contain the Non-Terminals and the column will contain the Terminal Symbols.
-            All the Null Productions of the Grammars will go under the Follow elements and the remaining productions
-            will lie under the elements of the First set
-
-        :return:
-        """
-
-c = "A"
-print(c[0])
+                # if iteration != 0:
+                #     stack.pop()
+                # iteration += 1
+                for symbol in production[::-1]:
+                    stack.append(symbol)
+        # If the stack is empty and we have processed all symbols in the sequence, the sequence is accepted
+        if not stack and i == len(sequence):
+            return True
+        # Otherwise, the sequence is not accepted
+        return False
 p = ParsingLL1(grammar='g1.txt',sequence='g1_seq.txt',out='out.txt')
-answer ={}
 
-print("FIRST SET",p.First("S", answer, skip=[]))
-# print("FIRST SET",p.First("a", answer, skip=[]))
+print("/// first and follow ///")
+print(p.first_set)
+print(p.follow_set)
+print("/// parsing table ///")
+print(p.parsing_table)
 
+print("/// started parsing ///")
+# print(p.parse())
 
-answer2 = {}
-
-print("FOLLOW SET", p.Follow("S",answer2, answer))
+if p.parse() == True:
+    print("Sequence accepted")
+else:
+    print("Sequence not accepted")
